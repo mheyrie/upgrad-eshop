81e351c9bac01d6b3f480e42259d5507b9b7e1ca
Mheyrie <omota4fidelity@gmail.com>
2024-11-03 13:55:58 +0100

    Sign Up

--- src/pages/auth/Signup.js
+++ src/pages/auth/Signup.js
@@ -1,4 +1,34 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import LockIcon from "@material-ui/icons/Lock";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import Button from "@material-ui/core/Button";
 export default function Signup() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [contactNumber, setContactNumber] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+   
+    if (password !== confirmPassword) {
+        setErrorMessage("Passwords do not match.");
+      } else {
+        setErrorMessage("");
+                console.log("Signup successful!", { password, confirmPassword });
+      }
+  };
+
+  // Toggle between showing and hiding password
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
     <div className="mt-5 d-flex flex-column justify-content-center align-items-center login">
       <div className="p-2 bg-danger rounded-3">
@@ -11,42 +41,92 @@ export default function Signup() {
         <div className="text-center mx-4 pt-4">
           <span className="fw-semibold fs-2">Sign in</span>
         </div>
-        <form className="mx-4 mt-4" onSubmit={handleSubmit}>
+        <form
+          className="d-flex flex-column gap-2 mx-4 mt-4 "
+          onSubmit={handleSubmit}
+        >
           <div className="form-group">
-            <label htmlFor="email" className="font-weight-bold">
-              Email
-            </label>
+            <input
+              type="text"
+              id="firstName"
+              value={firstName}
+              onChange={(e) => setEmail(e.target.value)}
+              className="form-control p-2"
+              placeholder="First Name*"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              id="lastName"
+              value={lastName}
+              onChange={(e) => setEmail(e.target.value)}
+              className="form-control p-2"
+              placeholder="Last Name *"
+              required
+            />
+          </div>
+          <div className="form-group">
             <input
               type="email"
               id="email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
-              className="form-control"
-              placeholder="Enter your email"
+              className="form-control p-2"
+              placeholder="Email Address *"
               required
             />
           </div>
           <div className="form-group position-relative">
-            <label htmlFor="password" className="font-weight-bold">
-              Password
-            </label>
             <input
               type={showPassword ? "text" : "password"}
               id="password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               className="form-control"
-              placeholder="Enter your password"
+              placeholder="Password *"
               required
             />
             <button
               type="button"
               onClick={togglePasswordVisibility}
               className="position-absolute end-0 translate-middle-y btn btn-link"
-              style={{ left: "350px", top: "30px" }}
+              style={{ left: "350px", top: "15px" }}
             >
               {showPassword ? <FaEyeSlash size={20} /> : <FaEye size={20} />}
             </button>
+          </div>
+          <div className="form-group position-relative">
+            <input
+              type={showPassword ? "text" : "password"}
+              id="password"
+              value={confirmPassword}
+              onChange={(e) => setPassword(e.target.value)}
+              className="form-control"
+              placeholder="Confirm Password *"
+              required
+            />
+            <button
+              type="button"
+              onClick={togglePasswordVisibility}
+              className="position-absolute end-0 translate-middle-y btn btn-link"
+              style={{ left: "350px", top: "15px" }}
+            >
+              {showPassword ? <FaEyeSlash size={20} /> : <FaEye size={20} />}
+            </button>
+            {errorMessage && <div className="text-danger mb-3">{errorMessage}</div>}
+          </div>
+          <div className="form-group">
+            <input
+              type="number"
+              id="contactNumber"
+              value={contactNumber}
+              onChange={(e) => setEmail(e.target.value)}
+              className="form-control p-2"
+              placeholder="Contact Number *"
+              required
+            />
           </div>
 
           <Button
